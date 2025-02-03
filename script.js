39f905328c2c8a476f5529b6f069c2e828c36f50
Sibusiso Nkabinde <sibusiso@ideapioneers.co.za>
2025-02-03 14:18:33 +0200

    made a few changes

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 document.getElementById("loadingSkipBtn").addEventListener("click", () => {
     document.getElementsByClassName("loading-container")[0].style.display = "none";
 })
+
+
+function randomValues () {
+    const values = []
+    let tot = 0
+    while (tot < screen.width) {
+        let value = Math.floor(Math.random() * 200) + 50;
+        tot += value;
+        values.push(value);
+    }
+
+    return values;
+}
+
+
+function shuffleSectionBackground () {
+    document.querySelectorAll(".first-section-background div").forEach( (div) => {
+        let values = randomValues()
+        div.innerHTML = "";
+    
+        for (let i=0; i<values.length; i++) {
+            let childDiv = document.createElement("div");
+            childDiv.className = "first-section-background-child";
+            childDiv.style.width = `${values[i]}px`;
+            div.appendChild(childDiv);
+        }
+    });
+}
+
+setInterval(shuffleSectionBackground, 3000);
